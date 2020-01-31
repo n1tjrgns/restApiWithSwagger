5f0497e37a55a6acbfc0b74525d6d6d9378d635b
n1tjrgns <n2tjrgns@gmail.com>
2020-01-31 15:28:38 +0900

    스프링 RestAPI 샘플 예제 + Swagger 적용

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -1,18 +1,23 @@
 package com.example.restservice;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
+@Api(value = "greeting Controller", description = "헬로 api")
 public class GreetingController {
+	//   http://localhost:8080/swagger-ui.html -> 실행 url
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
+	@ApiOperation(value = "hello, Id", notes = "헬로 id 를 반환하는 api")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
---
+++ complete/src/main/java/com/example/restservice/config/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.example.restservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    //   http://localhost:8080/swagger-ui.html -> 실행 url
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)// 응답메시지를 디폴트 값을 사용할건지
+                .globalResponseMessage(RequestMethod.GET, getArrayList());  //전반적인 반환 Message 셋팅
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "REST API Swagger 적용하기",
+                "스웨거 적용해보기",
+                "API TOS",
+                "Terms of service",
+                new Contact("n1tjrgns", "n1tjrgns.tistory.com", "n1tjrgns@naver.com"),
+                "", "", Collections.emptyList());
+    }
+
+    private ArrayList<ResponseMessage> getArrayList() {
+        ArrayList<ResponseMessage> lists = new ArrayList<>();
+
+        lists.add(new ResponseMessageBuilder().code(500).message("서버에러").responseModel(new ModelRef("Error")).build());
+        lists.add(new ResponseMessageBuilder().code(403).message("권한없음").responseModel(new ModelRef("Forbbiden")).build());
+        lists.add(new ResponseMessageBuilder().code(404).message("페이지 찾을 수 없음").responseModel(new ModelRef("PageNotFound")).build());
+
+        return lists;
+    }
+
+}
