d91e662bf0707ffaad6d067c7dea0a844648af09
n1tjrgns <n2tjrgns@gmail.com>
2020-07-28 17:48:44 +0900

    readonly 수정

--- complete/src/main/java/com/example/restservice/jpasample/MemberRepository.java
+++ complete/src/main/java/com/example/restservice/jpasample/MemberRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
+@Transactional(readOnly = true)
 @RepositoryRestResource
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
